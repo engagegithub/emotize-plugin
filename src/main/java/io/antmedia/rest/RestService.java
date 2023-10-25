e20bbedeb25b9a377c0bad5d1e75a36cad9888a2
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2023-10-25 13:17:38 +0300

    add sources

--- src/main/java/io/antmedia/rest/RestService.java
+++ src/main/java/io/antmedia/rest/RestService.java
@@ -7,7 +7,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,8 +15,6 @@ import javax.ws.rs.core.UriInfo;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
-import io.antmedia.rest.model.Result;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.google.gson.Gson;
